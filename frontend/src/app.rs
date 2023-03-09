8fcf5fd7998e9bc1b380b4bf93240744afd44e87
Jakob Naucke <jakob.naucke@posteo.de>
2023-03-09 16:03:57 +0100

    Basic status code showing
    Signed-off-by: Jakob Naucke <jakob.naucke@posteo.de>
    Co-authored-by: Jim van Vliet <buggstream@tuta.io>

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -6,26 +6,36 @@ use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let fire_some_data = Callback::from(move |_| {
-        let demo = ResultDTO {
-            model: "foo".to_string(),
-            user_agent: "bar".to_string(), // might require navigator via wasm_bindgen
-            benchmark_results: json!(null),
-            times: json!(null),
-        };
-        wasm_bindgen_futures::spawn_local(async move {
-            let _ = Request::post("/api/result/upload")
-                .json(&demo)
-                .unwrap()
-                .send()
-                .await
-                .unwrap();
-        });
-    });
+    let response_indicator = use_state(|| "".to_string());
+    let fire_some_data = {
+        let response_indicator = response_indicator.clone();
+        Callback::from(move |_| {
+            let response_indicator = response_indicator.clone();
+            let demo = ResultDTO {
+                model: "foo".to_string(),
+                user_agent: "bar".to_string(), // might require navigator via wasm_bindgen
+                benchmark_results: json!(null),
+                times: json!(null),
+            };
+            wasm_bindgen_futures::spawn_local(async move {
+                response_indicator.set(
+                    Request::post("/api/result/upload")
+                        .json(&demo)
+                        .unwrap()
+                        .send()
+                        .await
+                        .unwrap()
+                        .status()
+                        .to_string(),
+                );
+            });
+        })
+    };
 
     html! {
         <main>
             <button onclick={fire_some_data}>{"Fire some data"}</button>
+            <p>{&*response_indicator}</p>
         </main>
     }
 }
