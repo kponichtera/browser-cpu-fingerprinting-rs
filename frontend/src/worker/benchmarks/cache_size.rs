7a0d1acd315ea9ea72bd765f68f5d30dfab68ef1
Cornul11 <cornul11@gmail.com>
2023-03-28 20:01:00 +0200

    changed the sizes tested to mimic the original benchmark

--- frontend/src/worker/benchmarks/cache_size.rs
+++ frontend/src/worker/benchmarks/cache_size.rs
@@ -1,5 +1,7 @@
 use std::hint::black_box;
+use std::iter::StepBy;
 use std::mem::size_of;
+use std::ops::Range;
 
 use gloo_console::info;
 use rand::seq::SliceRandom;
@@ -13,25 +15,26 @@ const KB: usize = 1024;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct DataPoint {
-    x: u16,
+    x: usize,
     y: i64,
 }
 
 pub fn run_cache_size_benchmark(clock: Clock) -> BenchmarkResult {
     info!("Running cache size benchmark");
     let starting_time = clock.read().unwrap();
-    let l1 = (10..150).step_by(2);
-    let l2 = (150..2000).step_by(50);
-    let l3 = (2000..25000).step_by(500);
-    let sizes = [l1, l2, l3].map(|l| l.collect::<Vec<_>>()).concat();
-
+    let l1 = (4..512 + 1).step_by(4); // mimic the original sizes from the paper
+    // let l2 = (150..2000).step_by(50);
+    // let l3 = (2000..25000).step_by(500);
+    // let sizes = [l1, l2, l3].map(|l| l.collect::<Vec<_>>()).concat();
+    let mut sizes = [l1].map(|l| l.collect::<Vec<_>>()).concat();
+    sizes.push(1); // 1 KB
     let mut rand = rand::thread_rng();
     let result: Vec<DataPoint> = sizes
         .into_iter()
         .map(|s| {
             // this info may be removed at a later stage
             info!("Running cache-size profiler with size:", s);
-            let size = KB * s as usize / size_of::<usize>();
+            let size = s * KB / size_of::<usize>();
             let mut list = vec![0usize; size];
             let mut indices = (0..size).collect::<Vec<_>>();
             indices.shuffle(&mut rand);
@@ -52,10 +55,10 @@ pub fn run_cache_size_benchmark(clock: Clock) -> BenchmarkResult {
             p = 0;
             let start = clock.read().unwrap();
             for _ in 0..size {
-                p = black_box(list[p]);
+                p = black_box(list[black_box(p)]);
             }
             let end = clock.read().unwrap();
-            DataPoint { x: s, y: (end - start) / s as i64 }
+            DataPoint { x: s * KB, y: (end - start) }
         })
         .collect::<Vec<_>>();
 
